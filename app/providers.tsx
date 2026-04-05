531dcbfd0f0fdd9aa64771fd10798671d95f1fd5
Gustavo-Bau <gbau185@gmail.com>
2026-04-05 19:23:55 -0300

    Finalize JS migration and make project Vercel-deploy ready

--- app/providers.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { CartProvider } from '@/contexts/CartContext';
-
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <CartProvider>{children}</CartProvider>;
-}
